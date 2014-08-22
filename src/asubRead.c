38a3c202719776ed0bd3f9fcda047547148a9f66
Simon Gregor Ebner <simon.ebner@psi.ch>
2014-08-22 13:59:15 +0200

    fixed debug statument

--- src/asubRead.c
+++ src/asubRead.c
@@ -40,7 +40,7 @@ static long bsreadRead(aSubRecord *prec) {
 		resourceListItem* currentNode = resourceList;
 
 		do {
-			printf("Read > %s\n",m.key);
+			printf("Read > %d\n",items);
 			dbGetField(&currentNode->res.address, DBR_DOUBLE, &m.values[items], NULL, NULL, NULL);
 			currentNode = currentNode->next;
 			items++;
