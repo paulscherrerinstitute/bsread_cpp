8088c7a2cd1cea9d8111f85839cbf03f5c6bfe3c
Tom Slejko <you@example.com>
2015-11-10 15:16:02 +0100

    Chagned default port to 9999

--- bsread_sim.cmd
+++ bsread_sim.cmd
@@ -14,4 +14,4 @@ addScan .01
 dbLoadRecords "bsread_sim.template","P=$(SYS)-BSREAD"
 dbLoadRecords "bsread.template", "P=$(SYS)-BSREAD,BSREAD_PULSEID=$(BSREAD_PULSEID=$(SYS)-BSREAD:SIM-PULSE),BSREAD_TS_SEC=$(BSREAD_TS_SEC=$(SYS)-BSREAD:SIM-TS-SEC),BSREAD_TS_NSEC=$(BSREAD_TS_NSEC=$(SYS)-BSREAD:SIM-TS-SEC)"
 
-bsreadConfigure("default",9995,"PUSH")
+bsreadConfigure("default",9999,"PUSH")
