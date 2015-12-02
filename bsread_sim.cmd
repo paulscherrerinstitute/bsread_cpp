21acfd9d6ffabb748fc878e8f6afa7675028c08a
ebner <simon.ebner@psi.ch>
2015-12-02 14:18:46 +0100

    updated examples and made PORT easily configurable

--- bsread_sim.cmd
+++ bsread_sim.cmd
@@ -1,12 +1,14 @@
 ##BSREAD SIMULATION MODE
-## Mandatory macros: 
-##	
+## Mandatory macros:
+##
 ##	- SYS
 ##
 ## Optional macros [default]
-##	- BSREAD_PULSEID 
+##  - BSREAD_PORT [9999]
+##	- BSREAD_PULSEID
 ##	- BSREAD_TS_SEC
 ##	- BSREAD_TS_NSEC
+
 
 require misc
 require bsread
@@ -14,4 +16,4 @@ addScan .01
 dbLoadRecords "bsread_sim.template","P=$(SYS)-BSREAD"
 dbLoadRecords "bsread.template", "P=$(SYS)-BSREAD,BSREAD_PULSEID=$(BSREAD_PULSEID=$(SYS)-BSREAD:SIM-PULSE),BSREAD_TS_SEC=$(BSREAD_TS_SEC=$(SYS)-BSREAD:SIM-TS-SEC),BSREAD_TS_NSEC=$(BSREAD_TS_NSEC=$(SYS)-BSREAD:SIM-TS-SEC)"
 
-bsreadConfigure("default",9999,"PUSH")
+bsreadConfigure("default",$(BSREAD_PORT=9999),"PUSH")
--- example/startup_sim.cmd
+++ example/startup_sim.cmd
@@ -1,6 +1,5 @@
 require "bsread"
 
-runScript $(bsread_DIR)/bsread_sim.cmd, "SYS=SLEJKO-TEST"
+runScript $(bsread_DIR)/bsread_sim.cmd, "SYS=SOME-TEST,BSREAD_PORT=9999"
 
-dbLoadRecords("bsread_test.template","P=SLEJKO-TEST-FAKEDATA")
-
+dbLoadRecords("bsread_test.template","P=SOME-TEST-FAKEDATA")
