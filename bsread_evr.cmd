9ceebcb64216ff73996621b72586d4b14188e1f1
Tom Slejko <you@example.com>
2016-07-26 11:26:26 +0200

    Changed default bsread event to 28

--- bsread_evr.cmd
+++ bsread_evr.cmd
@@ -7,6 +7,7 @@
 ##  - BSREAD_PORT [9999]
 ##  - BSREAD_MODE [PUSH]
 ##	- EVR [EVR0]
+##      - BSREAD_EVENT
 ##	- BSREAD_PULSEID
 ##	- BSREAD_TS_SEC
 ##	- BSREAD_TS_NSEC
@@ -17,7 +18,7 @@
 
 
 require bsread
-$(NO_EVR=)dbLoadRecords "bsread_evr.template", "P=$(SYS)-BSREAD,EVR=$(EVR=EVR0),BSREAD_EVENT=$(BSREAD_EVENT=40)"
+$(NO_EVR=)dbLoadRecords "bsread_evr.template", "P=$(SYS)-BSREAD,EVR=$(EVR=EVR0),BSREAD_EVENT=$(BSREAD_EVENT=28)"
 dbLoadRecords "bsread.template", "P=$(SYS)-BSREAD,BSREAD_PULSEID=$(BSREAD_PULSEID=$(SYS)-$(EVR):RX-PULSEID),BSREAD_TS_SEC=$(BSREAD_TS_SEC=$(SYS)-$(EVR):RX-MTS-SEC),BSREAD_TS_NSEC=$(BSREAD_TS_NSEC=$(SYS)-$(EVR):RX-MTS-NSEC),READ_FLNK=$(READ_FLNK=)"
 
 bsreadConfigure("default",$(BSREAD_PORT=9999),$(BSREAD_MODE=PUSH))
