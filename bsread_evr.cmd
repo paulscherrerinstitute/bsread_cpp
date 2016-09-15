4e448d2dcd049d53224a5fd1d8b8f30fc4a8168f
Dirk Zimoch <dirk.zimoch@psi.ch>
2016-09-15 14:46:59 +0200

    use IOC name as default for SYS

--- bsread_evr.cmd
+++ bsread_evr.cmd
@@ -1,23 +1,21 @@
 ##BSREAD USING EVR
-## Mandatory parameters/macros:
-##
-##	- SYS
 ##
 ## Optional parameters/macros [default]
+##  - SYS [$(IOC)]
 ##  - BSREAD_PORT [9999]
 ##  - BSREAD_MODE [PUSH]
 ##	- EVR [EVR0]
 ##  - BSREAD_EVENT [28]
-##	- BSREAD_PULSEID [$(SYS)-$(EVR):RX-PULSEID]
-##	- BSREAD_TS_SEC [$(SYS)-$(EVR):RX-MTS-SEC]
-##	- BSREAD_TS_NSEC [$(SYS)-$(EVR):RX-MTS-NSEC]
+##	- BSREAD_PULSEID [$(SYS=$(IOC))-$(EVR):RX-PULSEID]
+##	- BSREAD_TS_SEC [$(SYS=$(IOC))-$(EVR):RX-MTS-SEC]
+##	- BSREAD_TS_NSEC [$(SYS=$(IOC))-$(EVR):RX-MTS-NSEC]
 ##  - READ_FLNK []
 ##  - PULSEID_OFFSET [0]
 ##
 ## To disable the loading of the EVR template use following parameter/macro: NO_EVR=#
 
 require bsread
-$(NO_EVR=)dbLoadRecords "bsread_evr.template", "P=$(SYS)-BSREAD,EVR=$(EVR=EVR0),BSREAD_EVENT=$(BSREAD_EVENT=28)"
-dbLoadRecords "bsread.template", "P=$(SYS)-BSREAD,PULSEID_OFFSET=$(PULSEID_OFFSET=0),BSREAD_PULSEID=$(BSREAD_PULSEID=$(SYS)-$(EVR):RX-PULSEID),BSREAD_TS_SEC=$(BSREAD_TS_SEC=$(SYS)-$(EVR):RX-MTS-SEC),BSREAD_TS_NSEC=$(BSREAD_TS_NSEC=$(SYS)-$(EVR):RX-MTS-NSEC),READ_FLNK=$(READ_FLNK=)"
+$(NO_EVR=)dbLoadRecords "bsread_evr.template", "P=$(SYS=$(IOC))-BSREAD,EVR=$(EVR=EVR0),BSREAD_EVENT=$(BSREAD_EVENT=28)"
+dbLoadRecords "bsread.template", "P=$(SYS=$(IOC))-BSREAD,PULSEID_OFFSET=$(PULSEID_OFFSET=0),BSREAD_PULSEID=$(BSREAD_PULSEID=$(SYS=$(IOC))-$(EVR):RX-PULSEID),BSREAD_TS_SEC=$(BSREAD_TS_SEC=$(SYS=$(IOC))-$(EVR):RX-MTS-SEC),BSREAD_TS_NSEC=$(BSREAD_TS_NSEC=$(SYS=$(IOC))-$(EVR):RX-MTS-NSEC),READ_FLNK=$(READ_FLNK=)"
 
 bsreadConfigure("default",$(BSREAD_PORT=9999),$(BSREAD_MODE=PUSH))
