ebdc689daaeafabb3b3cd83fd8f8ac3359bc97a5
Andrej Babic <andrej.babic@psi.ch>
2018-08-27 11:00:10 +0200

    Extract bsread sender

---
+++ src/BSDataSenderZmq.cpp
@@ -0,0 +1,92 @@
+#include "BSDataSenderZmq.h"
+
+using namespace std;
+
+bsread::BSDataSenderZmq::BSDataSenderZmq(zmq::context_t &ctx, string address, int sndhwm, int sock_type, int linger):
+        m_compress_buffer_size(0),
+        m_ctx(ctx),
+        m_sock(m_ctx, sock_type),
+        m_address(address.c_str())
+{
+    m_sock.setsockopt(ZMQ_LINGER, &linger,sizeof(linger));
+    m_sock.setsockopt(ZMQ_SNDHWM, &sndhwm,sizeof(sndhwm));
+    m_sock.bind(address.c_str());
+}
+
+bsread::BSDataSenderZmq::~BSDataSenderZmq() {}
+
+size_t bsread::BSDataSenderZmq::send_message(bsread::BSDataMessage &message, zmq::socket_t& sock,char*& compress_buffer, size_t &compress_buffer_size){
+    size_t msg_len=0;
+    size_t part_len;
+
+    //Send main header
+    const string* mainheader = message.get_main_header();
+    part_len = sock.send(mainheader->c_str(),mainheader->length(),ZMQ_SNDMORE|ZMQ_NOBLOCK);
+    if(!part_len) return 0;
+    msg_len+=part_len;
+
+
+    //Send dataheader
+    const string* dataheader = message.get_data_header(true);
+    part_len = sock.send(dataheader->c_str(),dataheader->length(),ZMQ_SNDMORE|ZMQ_NOBLOCK);
+    if(!part_len) return 0;
+    msg_len+=part_len;
+
+    //Send data for each channel
+    const vector<BSDataChannel*>* channels = message.get_channels();
+    size_t n = channels->size();
+    for(size_t i=0; i<n; i++){
+        int zmq_flags = ZMQ_SNDMORE | ZMQ_NOBLOCK;
+        BSDataChannel* chan = channels->at(i);
+
+        //Only send enabled channels
+        if(chan->get_enabled()){
+            const void* data;
+            size_t data_len;
+            //Fetch timestamp
+            uint64_t rtimestamp[2];
+            chan->get_timestamp(rtimestamp);
+
+
+            //Acquire and send data
+
+            //Check if compressing is enabled
+            /* When sending compressed data we are sending from our buffer (acquire_compressed makes a
+             * compressed copy of the source data. As such it does not require unlocking of channel.
+             * This is the reason for different handling of compressed vs uncompressed data here */
+            data_len = chan->acquire_compressed(compress_buffer,compress_buffer_size);
+            if(data_len){
+                part_len = sock.send(compress_buffer,data_len,zmq_flags);
+                msg_len+=part_len;
+            }
+            else{ //Compressing disabled
+                data = chan->acquire();
+                data_len = chan->get_len();
+
+                part_len = sock.send(data,data_len,zmq_flags);
+                msg_len+=part_len;
+            }
+
+
+            //Send timestamp, take care of last part
+            if(i==n-1) zmq_flags &= ~ZMQ_SNDMORE;
+            part_len = sock.send(rtimestamp,sizeof(rtimestamp),zmq_flags);
+
+            msg_len+=part_len;
+
+        }
+            //Not enabled channels are replaced with empty submessages
+        else{
+            sock.send(NULL,0,ZMQ_SNDMORE | ZMQ_NOBLOCK);
+
+            //Last part
+            if(i==n-1) zmq_flags &= ~ZMQ_SNDMORE;
+            part_len = sock.send(NULL,0,zmq_flags);
+
+        }
+
+    }
+
+    return msg_len;
+}
+
---
+++ src/BSDataSenderZmq.h
@@ -0,0 +1,47 @@
+#ifndef LIB_BSREAD_BSDATASENDERZMQ_H
+#define LIB_BSREAD_BSDATASENDERZMQ_H
+
+#include <cstddef>
+#include <string>
+#include <zmq.hpp>
+
+#include "BSDataMessage.h"
+
+namespace bsread{
+    /**
+     * @brief The BSDataSenderZmq class
+     *
+     * Class that takes BSData messages and transmits them via ZMQ socket
+     * By default the class will create its own context and socket. A static
+     * method exists that perorms the same, but requires ZMQ socket to be passed
+     */
+    class BSDataSenderZmq{
+
+    protected:
+        char* m_compress_buffer;
+        size_t m_compress_buffer_size;
+        zmq::context_t& m_ctx;
+        zmq::socket_t m_sock;
+        std::string m_address;
+
+    public:
+
+        /**
+         * @brief BSDataSenderZmq Holds all infrastructre needed for BSREAD, this includes zmq context and zmq socket.
+         * Parameters passed to constructor are used to create this infrastructure.
+         * @param address
+         * @param sndhwm
+         * @param sock_type
+         * @param linger
+         */
+        BSDataSenderZmq(zmq::context_t& ctx, std::string address,
+                        int sndhwm=10, int sock_type=ZMQ_PUSH, int linger=1000);
+
+        virtual ~BSDataSenderZmq();
+
+        virtual size_t send_message(BSDataMessage& message, zmq::socket_t &sock, char*& compress_buffer,
+                                    size_t &compress_buffer_size);
+    };
+}
+
+#endif //LIB_BSREAD_BSDATASENDERZMQ_H
