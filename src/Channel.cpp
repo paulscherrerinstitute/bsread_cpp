039458343379c6dc267eb0a4c74fd5d5f06cc680
Andrej Babic <andrej.babic@psi.ch>
2018-08-31 15:21:06 +0200

    Channel cleanup

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -4,117 +4,58 @@
 
 using namespace std;
 
-//Simple code to allow runtime detection of system endianess
-bool isLittleEndian()
+bsread::Channel::Channel(const string &name, bsread::bsdata_type type, vector<size_t> shape,
+                         endianess endian, compression_type compression, int modulo, int offset):
+        m_name(name),
+        m_type(type),
+        m_endianess(endian),
+        m_endianess_name(endianess_names.at(endian)),
+        m_compression(compression),
+        m_compression_name(compression_type_names.at(compression)),
+        m_shape(shape),
+        m_modulo(modulo),
+        m_offset(offset)
 {
-    short int number = 0x1;
-    char *numPtr = (char*)&number;
-    return (numPtr[0] == 1);
+    if (m_modulo < 1) {
+        throw runtime_error("Modulo cannot be less than 1.");
+    }
+
+    if (shape.size() == 0) {
+        throw runtime_error("Shape must have at least 1 value.");
+    }
 }
 
-Json::Value bsread::Channel::get_data_header(){
-    Json::Value root;
-    root["name"]=m_name;
-    root["offset"] = m_meta_offset;
-    root["modulo"] = m_meta_modulo;
-    root["type"]= bsdata_type_name[m_type];
-    root["encoding"]= m_encoding_le ? "little" : "big";
-    root["compression"] = compression_names.at(m_compression);
+string bsread::Channel::get_name() const {
+    return m_name;
+}
 
-    if(m_shape.size()){
-        for(unsigned int i=0;i<m_shape.size();i++){
-            root["shape"][i]=m_shape[i];
-        }
-    }
-    else{
-        root["shape"][0]=static_cast<int>(m_len); //shape is array of dimensions, scalar = [1]
+Json::Value bsread::Channel::get_channel_data_header() const {
+    Json::Value root;
+    root["name"] = m_name;
+    root["type"]= bsdata_type_name[m_type];
+    root["encoding"]= m_endianess_name;
+    root["compression"] = m_compression_name;
+    root["modulo"] = m_modulo;
+    root["offset"] = m_offset;
+
+    for (uint16_t i = 0; i<m_shape.size(); i++) {
+        root["shape"][i] = static_cast<int>(m_shape[i]);
     }
 
     return root;
 }
 
-bool bsread::Channel::is_enabled_for_pulse_id(uint64_t pulse_id){
-//    if(set_enable){
-//    for(size_t i=0;i<m_channels.size();i++){
-//    Channel* c = m_channels[i];
-//
-//    int modulo = c->m_meta_modulo;
-//
-//    int offset = c->m_meta_offset;
-//
-//    //Calculate modulo if set
-//    if (modulo > 0) {
-//    if ( ((pulseid-offset) % modulo ) == 0) {
-//    c->set_enabled(true);
-//    any_channels_enabled = true;
-//    }
-//    else{
-//    c->set_enabled(false);
-//    }
-//    }
-//    }
-    return true;
+void* bsread::Channel::get_data_for_pulse_id(uint64_t pulse_id) {
+
+    if (!is_enabled_for_pulse_id(pulse_id)) {
+        return nullptr;
     }
 
-size_t bsread::Channel::acquire_compressed(char*& buffer, size_t& buffer_size){
-    if(m_compression==compression_none) return 0;
+    // TODO: Return the value from the value provider.
 
-    const void* uncompressed_data = acquire(); //Do not forget to release before exiting this function
-    int32_t uncompressed_data_len = get_len();
-    size_t compressed_size=0;
-
-    if(m_compression==compression_lz4){
-        compressed_size=compress_lz4((const char*)uncompressed_data,uncompressed_data_len,buffer,buffer_size,false);
-    }
-    if(m_compression==compression_bslz4){
-        compressed_size=compress_bitshuffle((const char*)uncompressed_data,get_nelm(),get_elem_size(),buffer,buffer_size);
-    }
-
-    return compressed_size;
+    return nullptr;
 }
 
-string bsread::Channel::dump_header(){
-    return get_data_header().toStyledString();
+bool bsread::Channel::is_enabled_for_pulse_id(uint64_t pulse_id) const {
+    return ((pulse_id-m_offset) % m_modulo) == 0;
 }
-
-void bsread::Channel::set_enabled(bool enabled){
-    m_enabled = enabled;
-}
-
-bool bsread::Channel::get_enabled(){
-    return m_enabled;
-}
-
-string bsread::Channel::get_name(){
-    return m_name;
-}
-
-bsread::Channel::Channel(const string &name, bsread::bsdata_type type, const string):
-        m_type(type),
-        m_data(0),
-        m_timestamp(),
-        m_len(0),
-        m_name(name),
-        m_encoding_le(isLittleEndian()),
-        m_compression(compression_none),
-        m_enabled(true),
-        m_callback(0),
-        m_meta_modulo(1),
-        m_meta_offset(0)
-{}
-
-size_t bsread::Channel::set_data(void *data, size_t len){
-    m_data=data;
-    m_len=len;
-
-    size_t n_bytes = get_len();
-
-    // TODO: Lock the buffer.
-    if ( data_buffer.get() == nullptr || n_bytes > data_buffer_length ) {
-        data_buffer.reset(new char[n_bytes]);
-        data_buffer_length = n_bytes;
-    }
-
-    return n_bytes;
-}
-
--- src/Channel.h
+++ src/Channel.h
@@ -8,147 +8,35 @@
 #include "json.h"
 #include "constants.h"
 
-
-
 namespace bsread {
-
 
     class Channel {
 
     protected:
         const std::string m_name;
         const bsdata_type m_type;
-        const std::string endianess;
-
-
-        void*           m_data;
-        timestamp       m_timestamp;
-        size_t          m_len;
-
-
-        int             m_meta_modulo;
-        int             m_meta_offset;
-        bsdata_compression_type    m_compression;
-        bool            m_enabled;
-        std::vector<unsigned int> m_shape;
-
-        std::unique_ptr<char> data_buffer;
-        size_t data_buffer_length = 0;
+        const std::vector<size_t> m_shape;
+        const endianess m_endianess;
+        const std::string m_endianess_name;
+        const compression_type m_compression;
+        const std::string m_compression_name;
+        const int m_modulo;
+        const int m_offset;
 
     public:
+        Channel(const std::string &name, bsread::bsdata_type type, std::vector<size_t> shape,
+                endianess endian, compression_type compression=compression_none, int modulo=1, int offset=0);
 
-        Channel(const std::string& name, bsdata_type type);
+        std::string get_name() const;
 
+        Json::Value get_channel_data_header() const;
 
-        /**
-         * @brief get_data_header
-         * @param config_only if set to true the data header returend contains
-         *  only configuration fields (name, modulo, offset)
-         * @return Json::Value representing data header for this channel
-         */
-        Json::Value get_data_header();
+        void* get_data_for_pulse_id(uint64_t pulse_id);
 
-        bool is_enabled_for_pulse_id(uint64_t pulse_id);
-
-
-        /* extra metadata variables */
-        Json::Value     m_meta;
-
-
-
-        /**
-         * @brief set_data set message data. length is specified in number of
-         * elements and not size in bytes. It is considered that data points
-         * to an array of elements of type specified in constructor.
-         * @param data
-         * @param len number of elements of type m_type.
-         * @return expected size of data (e.g. len*sizeof(m_type))
-         */
-        size_t set_data(void* data, size_t len);
-
-        void* get_data(){
-            return m_data;
-        }
-
-        /**
-         * @brief get_len
-         * @return size of data returned by acquire in bytes
-         */
-        inline size_t get_len(){
-            return get_nelm()*get_elem_size();
-        }
-
-        /**
-         * @brief get_nelm
-         * @return number of elements in the channel
-         */
-        inline size_t get_nelm(){
-            return m_len;
-        }
-
-        /**
-         * @brief get_elem_size
-         * @return size of each element in bytes
-         */
-        inline size_t get_elem_size(){
-            return bsdata_type_size[m_type];
-        }
-
-        void set_timestamp(timestamp timestamp);
-
-        void set_timestamp(int64_t sec, int64_t nsec){
-            m_timestamp.sec=sec;
-            m_timestamp.nsec = nsec;
-        }
-
-
-        timestamp get_timestamp(){
-            return m_timestamp;
-        }
-
-        void get_timestamp(uint64_t dest[2]){
-            dest[0] = m_timestamp.sec;
-            dest[1] = m_timestamp.nsec;
-        }
-
-        void set_enabled(bool enabled);
-        bool get_enabled();
-
-        /**
-         * @brief set_shape sets the shape of the channel. If not specified (or if given an empty vector)
-         * than the shape is deducted automatically from size of data. Note that total size must remain
-         * unchanged or else the clients will not be able to parse the data.
-         *
-         * @param shape
-         */
-        void set_shape(const std::vector<unsigned int>& shape){
-            m_shape = shape;
-        }
-
-
-        /**
-         * @brief set_shape same as set_shape(); but with classic array interface that is more suitable for
-         * static configurations
-         * @param shape
-         * @param ndim
-         */
-        void set_shape(unsigned int shape[], size_t ndim){
-            m_shape.clear();
-            for(size_t i=0;i<ndim;i++){
-                m_shape.push_back(shape[i]);
-            }
-        }
-
-        std::string get_name();
-
-        std::string dump_header();
-
-
+    private:
+        bool is_enabled_for_pulse_id(uint64_t pulse_id) const;
 
     };
 }
-
-
-
 
 #endif //LIB_BSREAD_BSDATACHANNEL_H
