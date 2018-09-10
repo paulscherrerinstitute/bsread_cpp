e84fbb6204d14b86ff82634212a6aae3f078afdc
Andrej Babic <andrej.babic@psi.ch>
2018-09-10 11:50:26 +0200

    Reorder

--- src/DirectDataProvider.cpp
+++ src/DirectDataProvider.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 using namespace bsread;
 
-DirectDataProvider::DirectDataProvider(void* data, size_t data_len) : m_data(data), m_data_len(data_len)
+DirectDataProvider::DirectDataProvider(void* data, size_t data_len) :
+        m_data(data),
+        m_data_len(data_len)
 {}
 
 channel_data DirectDataProvider::get_data() {
