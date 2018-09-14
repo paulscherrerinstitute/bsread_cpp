ac1994c0e7fa736e22a5affdf004f0b71d2f0dd7
Andrej Babic <andrej.babic@psi.ch>
2018-09-14 17:12:58 +0200

    Move channel data to DataProvider

--- src/DataProvider.h
+++ src/DataProvider.h
@@ -4,6 +4,35 @@
 #include "constants.h"
 
 namespace bsread {
+
+    struct channel_data{
+
+        // Empty channel_data can be sent over ZMQ as an empty message.
+        channel_data():
+                data(nullptr),
+                data_len(0),
+                timestamp(nullptr),
+                timestamp_len(0)
+        {}
+
+        // Channel data can also be sent without a timestamp.
+        channel_data(void* data, size_t data_len) : data(data), data_len(data_len),
+                                                    timestamp(nullptr), timestamp_len(0)
+        {}
+
+        channel_data(void* data, size_t data_len, void* timestamp, size_t timestamp_len):
+                data(data),
+                data_len(data_len),
+                timestamp(timestamp),
+                timestamp_len(timestamp_len)
+        {}
+
+        void* data;
+        size_t data_len;
+        void* timestamp;
+        size_t timestamp_len;
+    };
+
     class DataProvider {
     public:
         virtual ~DataProvider() = 0;
--- src/constants.h
+++ src/constants.h
@@ -67,35 +67,6 @@ namespace bsread {
         uint64_t nsec;
     };
 
-    struct channel_data{
-
-        // Empty channel_data can be sent over ZMQ as an empty message.
-        channel_data():
-                data(nullptr),
-                data_len(0),
-                timestamp(nullptr),
-                timestamp_len(0)
-        {}
-
-        // Channel data can also be sent without a timestamp.
-        channel_data(void* data, size_t data_len) : data(data), data_len(data_len),
-                                                    timestamp(nullptr), timestamp_len(0)
-        {}
-
-        channel_data(void* data, size_t data_len, void* timestamp, size_t timestamp_len):
-                data(data),
-                data_len(data_len),
-                timestamp(timestamp),
-                timestamp_len(timestamp_len)
-        {}
-
-        void* data;
-        size_t data_len;
-        void* timestamp;
-        size_t timestamp_len;
-    };
-
-
     enum bsdata_type {BSDATA_STRING,
         BSDATA_BOOl,
         BSDATA_FLOAT64,
