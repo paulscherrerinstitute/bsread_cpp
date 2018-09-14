6e0ec65bfee5f6395fd91ac3d251cbce8a9767ac
Andrej Babic <andrej.babic@psi.ch>
2018-09-14 14:59:04 +0200

    Add data header receiver to DummyReceiver

--- tests/DummyReceiver.cpp
+++ tests/DummyReceiver.cpp
@@ -20,14 +20,20 @@ shared_ptr<bsread::bsread_message> bsread::DummyReceiver::receive() {
 
     m_sock.recv(&msg);
     m_sock.getsockopt(ZMQ_RCVMORE, &more, &more_size);
+    if (!more) throw runtime_error("Data header expected after main header.");
+
     auto main_header = get_main_header(msg.data(), msg.size());
+
+    m_sock.recv(&msg);
+    m_sock.getsockopt(ZMQ_RCVMORE, &more, &more_size);
+    auto data_header = get_data_header(msg.data(), msg.size());
 
     while (more) {
         m_sock.recv(&msg);
         m_sock.getsockopt(ZMQ_RCVMORE, &more, &more_size);
     }
 
-    return make_shared<bsread::bsread_message>(main_header);
+    return make_shared<bsread::bsread_message>(main_header, data_header);
 }
 
 
@@ -46,4 +52,34 @@ std::shared_ptr<bsread::main_header> bsread::DummyReceiver::get_main_header(void
                                               root["global_timestamp"]["ns"].asUInt64());
 
     return main_header;
+}
+
+std::shared_ptr<bsread::data_header> bsread::DummyReceiver::get_data_header(void* data, size_t data_len) {
+    Json::Value root;
+    auto json_string = string(static_cast<char*>(data), data_len);
+    json_reader.parse(json_string, root);
+
+    auto data_header = make_shared<bsread::data_header>();
+    data_header->htype = root["htype"].asString();
+
+    // Even if your IDE shows an error, it actually works.
+    for(auto& channel : root["channels"]) {
+
+        data_channel channel_definition;
+        channel_definition.name = channel["name"].asString();
+        channel_definition.type = bsdata_type_mapping.at(channel.get("type", "float64").asString());
+
+        for(auto& dimension : channel.get("shape", {1})) {
+            channel_definition.shape.push_back(dimension.asUInt());
+        }
+
+        channel_definition.compression = compression_type_mapping.at(channel.get("compression", "none").asString());
+        channel_definition.endianess = endianess_mapping.at(channel.get("endianess", "little").asString());
+        channel_definition.modulo = channel.get("modulo", 1).asUInt();
+        channel_definition.offset = channel.get("offset", 0).asUInt();
+
+        data_header->channels[channel_definition.name] = channel_definition;
+    }
+
+    return data_header;
 }
--- tests/DummyReceiver.h
+++ tests/DummyReceiver.h
@@ -4,6 +4,7 @@
 #include <zmq.hpp>
 #include <string>
 #include <memory>
+#include <vector>
 #include "json.h"
 
 #include "../src/constants.h"
@@ -18,10 +19,29 @@ namespace bsread {
         uint64_t pulse_id;
     };
 
+    struct data_channel {
+        std::string name;
+        bsdata_type type;
+        std::vector<uint32_t> shape;
+        compression_type compression;
+        endianess endianess;
+        uint32_t modulo;
+        uint32_t offset;
+    };
+
+    struct data_header {
+        std::string htype;
+        std::map<std::string, data_channel> channels;
+    };
+
     struct bsread_message {
-        bsread_message(std::shared_ptr<main_header> main_header): main_header(main_header) {};
+        bsread_message(std::shared_ptr<main_header> main_header,
+                       std::shared_ptr<data_header> data_header): main_header(main_header),
+                                                                  data_header(data_header)
+        {};
 
         std::shared_ptr<main_header> main_header;
+        std::shared_ptr<data_header> data_header;
     };
 
     class DummyReceiver {
@@ -39,6 +59,7 @@ namespace bsread {
 
     private:
         std::shared_ptr<main_header> get_main_header(void* data, size_t data_len);
+        std::shared_ptr<data_header> get_data_header(void* data, size_t data_len);
     };
 }
 
