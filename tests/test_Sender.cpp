464413fe08640c44ab428fb0bb56a2c63b3123a1
Andrej Babic <andrej.babic@psi.ch>
2018-09-14 13:57:32 +0200

    Create test for disabling sending

--- tests/test_Sender.cpp
+++ tests/test_Sender.cpp
@@ -24,16 +24,61 @@ TEST(Sender, basic_workflow) {
                                             BSDATA_UINT64));
 
     DummyReceiver receiver("tcp://0.0.0.0:12345");
+    sleep(1);
+
+    for (int i=0; i < n_messages; i++) {
+        // Simulate 100Hz.
+        usleep(10000);
+        EXPECT_TRUE(sender.send_message(i, {}) == SENT);
+    }
+
+    for (int i=0; i < n_messages; i++) {
+        auto message = receiver.receive();
+        EXPECT_EQ(message->main_header->pulse_id, i);
+    }
+}
+
+TEST(Sender, disable_sending) {
+
+    size_t n_messages = 10;
+
+    Sender sender("tcp://127.0.0.1:12345", n_messages);
+    DummyReceiver receiver("tcp://0.0.0.0:12345");
+
+    // There are no channels, all messages should be SKIPPED.
+    for (int i=0; i < n_messages; i++) {
+        // Simulate 100Hz.
+        usleep(10000);
+
+        EXPECT_TRUE(sender.send_message(i, {})==SKIPPED);
+    }
+
+    float test_float = 12.34;
+    sender.add_channel(make_shared<Channel>("test_channel_float",
+                                            make_shared<DirectDataProvider>(&test_float, sizeof(float)),
+                                            BSDATA_FLOAT32));
+
+    sender.set_sending_enabled(false);
+
+    // Sending is disabled, all messages should be skipped.
+    for (int i=0; i < n_messages; i++) {
+        // Simulate 100Hz.
+        usleep(10000);
+
+        EXPECT_TRUE(sender.send_message(i, {})==SKIPPED);
+    }
+
+    sender.set_sending_enabled(true);
 
     for (int i=0; i < n_messages; i++) {
         // Simulate 100Hz.
         usleep(10000);
 
-        auto result = sender.send_message(i, {});
-        EXPECT_TRUE(result != 0);
+        EXPECT_TRUE(sender.send_message(i, {})==SENT);
     }
 
     for (int i=0; i < n_messages; i++) {
-        receiver.receive();
+        auto message = receiver.receive();
+        EXPECT_EQ(message->main_header->pulse_id, i);
     }
 }
