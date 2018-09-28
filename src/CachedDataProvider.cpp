4013a605d795b16eb1857803391aa111ae33b3cd
Andrej Babic <andrej.babic@psi.ch>
2018-09-28 12:23:48 +0200

    Improve CachedDataProvider initialization

--- src/CachedDataProvider.cpp
+++ src/CachedDataProvider.cpp
@@ -4,10 +4,10 @@ using namespace std;
 using namespace bsread;
 
 bsread::CachedDataProvider::CachedDataProvider(void* data, size_t data_len) :
-        DirectDataProvider(data, data_len)
-{
-    m_data_cache.reset(new char[data_len]);
-}
+
+        DirectDataProvider(data, data_len),
+        m_data_cache(new char[data_len])
+{}
 
 void bsread::CachedDataProvider::cache_data() {
     memcpy(m_data_cache.get(), m_data, m_data_len);
