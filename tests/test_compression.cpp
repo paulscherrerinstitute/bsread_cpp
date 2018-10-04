73fed3f02f65bdd744061b2ce6bcd1f5c8df5abd
Andrej Babic <andrej.babic@psi.ch>
2018-10-04 09:46:00 +0200

    Add test for lz4 decompression

--- tests/test_compression.cpp
+++ tests/test_compression.cpp
@@ -44,3 +44,44 @@ TEST(compression, compress_buffer) {
     EXPECT_TRUE(size_bslz4 <= uncompressed_size);
     EXPECT_TRUE(size_bslz4 > 0);
 }
+
+TEST(compression, decompress_lz4) {
+
+    size_t image_pixel_size = 2160 * 2560;
+    size_t n_element_bytes = sizeof(uint16_t);
+    size_t image_bytes = image_pixel_size * n_element_bytes;
+
+    auto populate_buffer = [](char* buffer, size_t buffer_size, uint16_t init_value) {
+        for (size_t index=0; index<buffer_size/2; index++) {
+            ((uint16_t*)buffer)[index] = init_value;
+        }
+    };
+
+    auto compare_buffers = [](char* original_buffer, char* new_buffer, size_t buffer_size) {
+        for (size_t index=0; index<buffer_size; index++) {
+            SCOPED_TRACE(index);
+            EXPECT_EQ(original_buffer[index], new_buffer[index]);
+
+            if (original_buffer[index] != new_buffer[index]) {
+                return;
+            }
+        }
+    };
+
+    auto compression_buffer_size = get_compression_buffer_size(compression_lz4, image_pixel_size, n_element_bytes);
+
+    unique_ptr<char[]> original_buffer(new char[image_bytes]);
+    unique_ptr<char[]> compressed_buffer(new char[compression_buffer_size]);
+    unique_ptr<char[]> decompressed_buffer(new char[image_bytes*2]);
+
+    populate_buffer(original_buffer.get(), image_bytes, 65000);
+
+    auto compressed_size = compress_lz4(original_buffer.get(), image_pixel_size, sizeof(uint16_t),
+            compressed_buffer.get(), compression_buffer_size);
+
+    auto decompressed_size = decompress_lz4(compressed_buffer.get(), compressed_size, decompressed_buffer.get());
+
+    EXPECT_EQ(image_bytes, decompressed_size);
+
+    compare_buffers(original_buffer.get(), decompressed_buffer.get(), image_bytes);
+}
